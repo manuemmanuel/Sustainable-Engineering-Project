1cc7851d8b36139f7bd08c477c847c60b7d788aa
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-12-03 17:54:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,6 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-dark_colours = {
-    'background': '#000000',
-    'text': '#FFFFFF',
-    'dropdown-background': '#333333',
-    'dropdown-text': '#000000',
-    'graph-background': '#1E1E1E',
-    'graph-line-color': '#00CC96',
-}
-
 light_colours = {
     'background': '#FFFFFF',
     'text': '#000000',
@@ -28,11 +19,12 @@ light_colours = {
     'dropdown-text': '#000000',
     'graph-background': '#E5E5E5',
     'graph-line-color': '#4285F4',
+    'box-border-color': '#000000',  # Border color for the report box
 }
 
 app.layout = html.Div([
     html.Div([
-        html.H1('Carbon Footprint Data Representation', id='title', style={'text-align': 'center', 'font-size': '2em', 'margin-bottom': '10px', 'font-family': 'monospace'}),
+        html.H1('Carbon Footprint Data Representation', id='title', style={'text-align': 'center', 'font-size': '3em', 'margin-bottom': '10px', 'font-family': 'monospace'}),
         dcc.Dropdown(
             id='country-dropdown',
             options=[{'label': country, 'value': country} for country in df['country'].unique()],
@@ -48,69 +40,43 @@ app.layout = html.Div([
             multi=False,
         ),
     ]),
-    dcc.Checklist(
-        id='theme-toggle',
-        options=[
-            {'label': 'Dark Mode', 'value': 'dark'},
-        ],
-        value=[],  # Default mode (empty list for Light Mode)
-        inline=True,
-        labelStyle={'display': 'block', 'margin-top': '10px', 'font-family': 'monospace'},
-    ),
     dcc.Graph(id='line-plot', style={'margin-top': '20px', 'margin-bottom': '20px'}),
-    html.Div(id='report', style={'margin-top': '20px', 'font-family': 'monospace', 'font-size': '1.5em'}),  # Increased font size
+    html.Div([
+        html.Div(id='report-box', children=[
+            html.H3('Report', style={'text-align': 'center', 'color': light_colours['text'], 'margin-bottom': '10px'}),
+            html.Div(id='report', style={'font-family': 'monospace', 'font-size': '1.5em'}),
+        ], style={'border': f'2px solid {light_colours["box-border-color"]}', 'padding': '10px', 'border-radius': '10px'}),
+    ], style={'margin-top': '20px'}),
     html.Footer('@ Sustainable Engineering Project by Manu Emmanuel, Felix Jobi, and Nagaraj Menon K S', id='footer', style={'text-align': 'center', 'margin-top': '30px', 'font-family': 'monospace'}),
-], id='main-div')
-
-# Dynamic styles for dark and light modes
-app.layout.children.append(dcc.Markdown(id='style', children=[]))
+], id='main-div', style={'background-color': '#FFFFFF', 'color': '#000000'})
 
 @app.callback(
     [Output('line-plot', 'figure'),
-     Output('report', 'children'),
-     Output('style', 'children')],
-    [Input('country-dropdown', 'value'),
-     Input('theme-toggle', 'value')]
+     Output('report', 'children')],
+    [Input('country-dropdown', 'value')]
 )
-def report(selected_country, selected_theme):
+def update_layout(selected_country):
     selected_data = df[df['country'] == selected_country]
-
-    if selected_theme == 'dark':
-        colours = dark_colours
-        dynamic_styles = f"""
-            #main-div {{
-                background-color: {colours['background']};
-                color: {colours['text']};
-            }}
-        """
-    else:
-        colours = light_colours
-        dynamic_styles = f"""
-            #main-div {{
-                background-color: {colours['background']};
-                color: {colours['text']};
-            }}
-        """
 
     fig = px.line(selected_data, x='year', y='co2_per_capita',
                   labels={'co2_per_capita': 'CO2 per Capita', 'year': 'Year'},
                   line_shape='linear')
 
     fig.update_layout(title=f'CO2 per Capita - {selected_country} Over Years',
-                      plot_bgcolor=colours['graph-background'],
-                      paper_bgcolor=colours['background'],
-                      font=dict(color=colours['text'], family='monospace'),
-                      xaxis=dict(linecolor=colours['text']),
-                      yaxis=dict(linecolor=colours['text']),
-                      hoverlabel=dict(bgcolor=colours['background'], font_color=colours['text']),
-                      legend=dict(title=dict(text='Country', font=dict(color=colours['text']))),
+                      plot_bgcolor=light_colours['graph-background'],
+                      paper_bgcolor=light_colours['background'],
+                      font=dict(color=light_colours['text'], family='monospace'),
+                      xaxis=dict(linecolor=light_colours['text']),
+                      yaxis=dict(linecolor=light_colours['text']),
+                      hoverlabel=dict(bgcolor=light_colours['background'], font_color=light_colours['text']),
+                      legend=dict(title=dict(text='Country', font=dict(color=light_colours['text']))),
                       showlegend=True)
 
-    fig.update_traces(line=dict(color=colours['graph-line-color']))
+    fig.update_traces(line=dict(color=light_colours['graph-line-color']))
 
     report_text = generate_report(selected_country, selected_data)
 
-    return fig, report_text, [dynamic_styles]
+    return fig, report_text
 
 def generate_report(country, data):
     start_year = data['year'].min()
